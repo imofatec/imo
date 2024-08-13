de890bee7b1fb1a883f8695f2509d9b5762d2b69
matheus <matheus.mariano9@fatec.sp.gov.br>
2024-08-12 21:32:44 -0300

    :recycle: refactor: novo design header

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,39 +1,59 @@
-import headerlogoinicial from "@/assets/headerlogoinicial.svg"
-import headerlogoazul from "@/assets/headerlogoazul.svg"
+import LogoIMO from "@/assets/LogoIMO.svg"
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react";
 import { Outlet } from "react-router-dom";
-import { useState } from "react"
 import { Link } from "react-router-dom";
 
-export default function Header() {
-    const [logo, setLogo] = useState(headerlogoinicial)
+const headerItens = ["Data Science", "Devops", "Hardware", "Design", "Back End", "Front End"] // contem todos os botoes do header ciano
 
-    function handleLogoHover(e) {
-        setLogo(e.type === 'mouseenter' ? headerlogoazul : headerlogoinicial)
-    }
+// import { Logo } from "@/components/ui/logo"  || talvez utilizar depois caso a logo seja decidida XD
+
+export default function Header() {
 
     return (
-        <>
-            <header className="flex justify-around items-center h-28 bg-custom-gray">
-                <div className="inline-flex space-x-20">
-                    <img src={logo} onMouseEnter={handleLogoHover} onMouseOut={handleLogoHover} className="cursor-pointer"></img>
-                    <div className="inline-flex items-center w-96 -space-x-8">
-                        <Search className="z-10" />
-                        <Input type="search" placeholder="Pesquise por qualquer coisa" className="text-center rounded-full shadow-md shadow-gray-500" />
-                    </div>
+        <header className="h-[8.5rem] flex flex-col">
+
+            <div className="bg-custom-header-dark-purple h-20 flex justify-around items-center">
+
+                <Link to="/">
+                    <img src={LogoIMO} className="cursor-pointer"></img>
+                </Link>
+
+                <div className=" w-[49rem] relative flex items-center ">
+                    <Search className="absolute left-3 text-white" />
+                    <div className="absolute left-12 w-[1px] h-5 bg-white"></div>
+                    <Input type="search"
+                        placeholder="Pesquise por qualquer coisa"
+                        className="pl-14 bg-custom-search-dark border-custom-search-dark text-white w-full placeholder:text-white"
+                    />
                 </div>
-                <div className="space-x-6">
-                    <Button className="border border-white">Todos os cursos</Button>
-                    <Button className="border border-white">Fazer login</Button>
-                    <Link to="/cadastro">
-                        <Button className="text-black bg-white">Cadastrar-se</Button>
-                    </Link>
+
+                <div className="flex align-middle items-center">
+                    <Button className="">
+                        Criar Conta
+                    </Button>
+                    <Button className="">
+                        Fazer Login
+                    </Button>
                 </div>
-            </header>
-            <Outlet />
-        </>
+
+            </div>
+
+            <div className="bg-custom-header-cyan min-h-14 flex flex-wrap justify-center items-center gap-9">
+
+                {
+                    // CODIGO PARA PEGAR TODOS OS ITENS DA CONSTANTE E RETORNA OS BOTOES
+
+                    headerItens.map(function (item) {
+                        return (
+                            <Button className="text-black">{item}</Button>
+                        )
+                    })
+                }
+
+            </div>
+        </header>
     )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -22,7 +22,10 @@ module.exports = {
         "custom-light-blue": "#1E64E5",
         "custom-gray": "#2D2F31",
         "custom-border-gray":"#374151",
-        "custom-text-gray": "#9CA3AF"
+        "custom-text-gray": "#9CA3AF",
+        "custom-header-dark-purple": "#0E0025",
+        "custom-header-cyan": "#2DEFD8",
+        "custom-search-dark": "#1F192A"
       },
       keyframes: {
         "accordion-down": {
