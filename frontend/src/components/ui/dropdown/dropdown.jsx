8018a2fa5a6e805a7d13dd31dff358b7dea18d5f
matheus <matheus.mariano9@fatec.sp.gov.br>
2024-09-21 22:29:43 -0300

    :sparkles: feat: Implementado a exibição de todos os cursos e categorias do banco de dados

--- frontend/src/components/ui/dropdown/dropdown.jsx
+++ frontend/src/components/ui/dropdown/dropdown.jsx
@@ -1,24 +1,17 @@
-import { Accordion } from "@/components/ui/dropdown/accordion"
-import ItemDropdown from "./itemdropdown"
+import { Accordion } from '@/components/ui/dropdown/accordion'
+import ItemDropdown from './itemdropdown'
 
-export function Dropdown() {
-
-  const nomeCursos = [
-    { title: "Data Science" },
-    { title: "Redes" },
-    { title: "Gestão" },
-    { title: "Design" },
-    { title: "Programação" },
-    { title: "Classificação" }
-  ]
-
+export function Dropdown({categorias}) {
   return (
-
     <Accordion type="single" collapsible className="text-center ">
-      {nomeCursos.map((dado, i) => {
+      {categorias.map((dado, i) => {
         return (
-          <div>
-            <ItemDropdown key={i} numItem={i} labelItem={dado.title} contItem={5}></ItemDropdown>
+          <div key={dado.id}>
+            <ItemDropdown
+              numItem={i}
+              labelItem={dado.name}
+              contItem={2}
+            ></ItemDropdown>
           </div>
         )
       })}
