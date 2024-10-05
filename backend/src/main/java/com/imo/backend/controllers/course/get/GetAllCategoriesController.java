c74a7b40ca4b83b1342045f4b33545ec61fa4910
abnercerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-10-05 00:10:18 -0300

    :recycle: refactor: Adiciona tags para controllers na Swagger UI

--- backend/src/main/java/com/imo/backend/controllers/course/get/GetAllCategoriesController.java
+++ backend/src/main/java/com/imo/backend/controllers/course/get/GetAllCategoriesController.java
@@ -1,6 +1,6 @@
 package com.imo.backend.controllers.course.get;
 
-import com.imo.backend.controllers.CourseController;
+import com.imo.backend.controllers.course.CourseController;
 import com.imo.backend.models.course.dtos.Category;
 import com.imo.backend.models.course.services.get.GetAllCategoriesService;
 import org.springframework.http.ResponseEntity;
