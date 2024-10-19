6c6db8183e12953aa2def4ddc1332a6a7cbfe323
Matheus <matheus.mariano9@fatec.sp.gov.br>
2024-10-19 18:26:10 -0300

    :ok_hand: style: Aprimora visual do dropdown com icone de triangulo

--- frontend/src/components/ui/header/dropdwnHeader.jsx
+++ frontend/src/components/ui/header/dropdwnHeader.jsx
@@ -4,6 +4,7 @@ import SettingsIcon from './icons/settingsIcon'
 import Certificate from './icons/certificate'
 import CoursesIcon from './icons/coursesIcon'
 import PlusHeader from './icons/plusheader'
+import DropdownTriangle from './icons/dropdownTriangle'
 import LogoutHeader from './icons/logoutHeader'
 import { Link } from 'react-router-dom'
 import api from '@/api/api'
@@ -11,7 +12,7 @@ import useFetchUserInfo from '@/hooks/useFetchUserInfo'
 export default function DropdownHeader({ isLoggedIn }) {
   const [showDropdown, setShowDropdown] = useState(false)
   const [hideTimeout, setHideTimeout] = useState(null)
-  const { urlImage, fetchUserInfo, userInfo } = useFetchUserInfo()
+  const { urlImage, fetchUserInfo } = useFetchUserInfo()
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -49,51 +50,56 @@ export default function DropdownHeader({ isLoggedIn }) {
             />
           </div>
           {showDropdown && (
-            <div
-              className="absolute top-full right-[-4.5rem] mt-1 w-48 bg-custom-header-dark-purple border border-white rounded-lg shadow-lg"
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <ul>
-                <Link to="/user/configurar-conta">
-                  <li className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black">
-                    <SettingsIcon />
-                    Configurações
-                  </li>
-                </Link>
+            <>
+              <div className='absolute top-[3rem] right-[0.75rem]'>
+                <DropdownTriangle />
+              </div>
+              <div
+                className="absolute top-full right-[-2rem] mt-6 w-48 bg-custom-header-dark-purple border border-white rounded-lg shadow-lg"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <ul>
+                  <Link to="/user/configurar-conta">
+                    <li className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black transition-colors duration-300 ease-in-out">
+                      <SettingsIcon />
+                      Configurações
+                    </li>
+                  </Link>
 
-                <Link to={'/'}>
-                  <li className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black">
-                    <Certificate />
-                    Meus Certificados
-                  </li>
-                </Link>
+                  <Link to={'/'}>
+                    <li className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black transition-colors duration-300 ease-in-out">
+                      <Certificate />
+                      Meus Certificados
+                    </li>
+                  </Link>
 
-                <Link to={'/'}>
-                  <li className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black">
-                    <CoursesIcon />
-                    Meus Cursos
-                  </li>
-                </Link>
+                  <Link to={'/'}>
+                    <li className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black transition-colors duration-300 ease-in-out">
+                      <CoursesIcon />
+                      Meus Cursos
+                    </li>
+                  </Link>
 
-                <Link to={'/criar-curso'}>
-                  <li className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black">
-                    <PlusHeader />
-                    Submeter Curso
+                  <Link to={'/criar-curso'}>
+                    <li className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black transition-colors duration-300 ease-in-out">
+                      <PlusHeader />
+                      Submeter Curso
+                    </li>
+                  </Link>
+                  <li
+                    className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black transition-colors duration-300 ease-in-out cursor-pointer"
+                    onClick={() => {
+                      localStorage.clear('token')
+                      window.location.reload()
+                    }}
+                  >
+                    <LogoutHeader />
+                    Sair
                   </li>
-                </Link>
-                <li
-                  className="flex items-center gap-1 px-4 py-2 hover:bg-custom-header-cyan hover:text-black cursor-pointer"
-                  onClick={() => {
-                    localStorage.clear('token')
-                    window.location.reload()
-                  }}
-                >
-                  <LogoutHeader />
-                  Sair
-                </li>
-              </ul>
-            </div>
+                </ul>
+              </div>
+            </>
           )}
         </>
       ) : (
