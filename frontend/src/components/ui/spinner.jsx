2c813c42eefbcd33c7f2a0cbf165e830babae12d
Matheus <matheus.mariano9@fatec.sp.gov.br>
2024-11-03 18:27:21 -0300

    :sparkles: feat: Adiciona loading screen com logo durante atualização de página

---
+++ frontend/src/components/ui/spinner.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Spinner(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="white" d="M2 12C2 6.477 6.477 2 12 2v3a7 7 0 0 0-7 7z"></path></svg>);
+}
