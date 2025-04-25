2e71254966a1c5c811e1aaaa63e61a7c5b21026a
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2025-04-25 20:27:02 -0300

    :sparkles: feat: Adiciona método para achar outbox pelo status e evento

--- backend/src/main/java/com/imo/backend/models/outbox/repositories/OutboxRepository.java
+++ backend/src/main/java/com/imo/backend/models/outbox/repositories/OutboxRepository.java
@@ -1,7 +1,12 @@
 package com.imo.backend.models.outbox.repositories;
 
 import com.imo.backend.models.outbox.Outbox;
+import com.imo.backend.models.outbox.OutboxEvent;
+import com.imo.backend.models.outbox.OutboxStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface OutboxRepository<T> extends MongoRepository<Outbox<T>, String> {
+  Page<Outbox<T>> findByStatusAndEvent(OutboxStatus status, OutboxEvent event, Pageable pageable);
 }
