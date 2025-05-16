e3bf1de6b3778e2fcbff65fa5705da6ee2c04f47
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2025-05-15 21:30:30 -0300

    :sparkles: feat: Adiciona parentId na entidade comentário e dto para resposta

--- backend/src/main/java/com/imo/backend/models/comments/dto/CommentDTO.java
+++ backend/src/main/java/com/imo/backend/models/comments/dto/CommentDTO.java
@@ -10,4 +10,6 @@ public class CommentDTO {
   @NotBlank(message = "O comentario nao pode ser vazio")
   @Size(max = 300, message = "O seu comentario precisa ter menos de 300 caracteres")
   private String comment;
+
+  private String parentId;
 }
