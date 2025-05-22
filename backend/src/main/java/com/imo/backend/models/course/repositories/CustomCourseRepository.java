e903442ec0c0699c3be15a45803a20ccd51265a8
daniel <daniel.cordeiro2@fatec.sp.gov.br>
2025-05-22 11:01:05 -0300

    :sparkles: feat: Adiciona metodos que alteram status do curso e delecao de aula

--- backend/src/main/java/com/imo/backend/models/course/repositories/CustomCourseRepository.java
+++ backend/src/main/java/com/imo/backend/models/course/repositories/CustomCourseRepository.java
@@ -6,4 +6,6 @@ import com.imo.backend.models.course.dtos.FieldsToUpdateCourse;
 public interface CustomCourseRepository {
 
     Course updateCourse(String token, String courseId, FieldsToUpdateCourse fieldsToUpdateCourse);
+    void updateCourseStatus(String token, String courseId, boolean isActive);
+    void deleteLessonFromCourse(String token, String courseId, String lessonId);
 }
