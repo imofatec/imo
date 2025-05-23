c044167eb74da3d287d60255f9922de1760be60d
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2025-05-23 18:43:24 -0300

    :sparkles: feat: Adiciona nova implementação de edição de curso por id

--- backend/src/main/java/com/imo/backend/models/course/dtos/FieldsToUpdateCourse.java
+++ backend/src/main/java/com/imo/backend/models/course/dtos/FieldsToUpdateCourse.java
@@ -1,21 +1,19 @@
 package com.imo.backend.models.course.dtos;
 
-import com.imo.backend.models.lessons.Lesson;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class FieldsToUpdateCourse {
 
+  @Size(min = 10, max = 100, message = "O nome do curso precisa ter de 10 a 100 caracteres")
   private String name;
-
-  private String level;
 
   private String category;
 
-  private String description;
+  private String level;
 
-  private List<Lesson> lessons;
+  @Size(min = 10, max = 300, message = "A descrição do curso precisa ter de 10 a 300 caracteres")
+  private String description;
 
 }
