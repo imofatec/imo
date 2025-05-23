36fb1d0dced107f8bcbf9010ccfc50e9f709fe80
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2025-05-23 18:42:15 -0300

    :sparkles: feat: Atualiza implementação de repositório customizado de cursos

--- backend/src/main/java/com/imo/backend/models/course/services/patch/interfaces/ToggleCourseActivationService.java
+++ backend/src/main/java/com/imo/backend/models/course/services/patch/interfaces/ToggleCourseActivationService.java
@@ -1,9 +1,11 @@
 package com.imo.backend.models.course.services.patch.interfaces;
 
+import com.imo.backend.models.course.dtos.CourseOverview;
+
 public interface ToggleCourseActivationService {
     
-    void execute(String token, String courseId, boolean isActive);
+    void execute(String courseId, boolean isActive);
     void activate(String token, String courseId);
     void deactivate(String token, String courseId);
-    void toggle(String token, String courseId);
+    CourseOverview toggle(String courseId);
 }
