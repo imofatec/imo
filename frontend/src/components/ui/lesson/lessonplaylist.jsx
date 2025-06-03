84dd30f2aaf303d2d48a4c6bce913c83e6c46877
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-06-03 18:23:14 -0300

    :sparkles: feat: Adiciona loading state checkbox de confirmar aula

--- frontend/src/components/ui/lesson/lessonplaylist.jsx
+++ frontend/src/components/ui/lesson/lessonplaylist.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { Separator } from '../separator'
 import { Checkbox } from '../dropdown/checkbox'
 import { useState, useEffect } from 'react'
+import Spinner from '@/components/ui/spinner'
 
 export default function LessonPlaylist({
   thumbLesson,
@@ -17,19 +18,26 @@ export default function LessonPlaylist({
 }) {
   const [isEnabled, setIsEnabled] = useState()
   const [isChecked, setIsChecked] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  
 
   useEffect(() => {
     console.log(progress)
-    if (!progress || loadingProgress ) return
+    if (!progress || loadingProgress) return
 
     setIsEnabled((indexLesson - 1) <= progress.lessonsWatched)
     setIsChecked((indexLesson - 1) < progress.lessonsWatched)
   }, [progress, indexLesson, loadingProgress])
 
-  const handleCheckboxChange = (checked) => {
+  const handleCheckboxChange = async (checked) => {
     if (!isChecked && checked && isEnabled) {
+      setIsLoading(true)
       setIsChecked(true)
-      onFinished()
+      try {
+        await onFinished()
+      } finally {
+        setIsLoading(false)
+      }
     }
   }
 
@@ -43,15 +51,20 @@ export default function LessonPlaylist({
           <img className="rounded-xl" src={thumbLesson}></img>{' '}
         </Link>
         <div
-          className={` flex items-center justify-center w-6 h-6 ${isChecked ? 'bg-custom-header-cyan' : 'bg-transparent'}`}
+          className={` flex items-center justify-center w-6 h-6 ${isChecked && !isLoading ? 'bg-custom-header-cyan' : 'bg-transparent'}`}
         >
-          <Checkbox
-            id={indexLesson}
-            className="hover:scale-105 duration-200 w-6 h-6"
-            checked={isChecked}
-            onCheckedChange={handleCheckboxChange}
-            disabled={!isEnabled || isChecked}
-          />
+          {isLoading
+            ?
+            <Spinner className="animate-spin" />
+            :
+            <Checkbox
+              id={indexLesson}
+              className="hover:scale-105 duration-200 w-6 h-6"
+              checked={isChecked}
+              onCheckedChange={handleCheckboxChange}
+              disabled={!isEnabled || isChecked}
+            />
+          }
         </div>
       </div>
 
