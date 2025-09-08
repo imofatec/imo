7b840574ad7f56fdc1745bdf7a443d5cdc374801
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2025-09-08 11:39:43 -0300

    :books: docs: Adiciona scalar ui

--- backend/src/main/java/com/imo/backend/config/openapi/OpenAPIConfiguration.java
+++ backend/src/main/java/com/imo/backend/config/openapi/OpenAPIConfiguration.java
@@ -8,9 +8,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
+@Profile("dev")
 @SecurityScheme(
         name = "Authorization",
         type = SecuritySchemeType.HTTP,
