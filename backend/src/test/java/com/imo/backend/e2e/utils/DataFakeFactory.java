6ac92e1be2a73ef1fc20f1cb7ad3874a0d79d0cb
daniel <daniel.cordeiro2@fatec.sp.gov.br>
2025-09-28 15:35:37 -0300

    ✨ feat: Adiciona métodos utilitários para criacao de um curso

--- backend/src/test/java/com/imo/backend/e2e/utils/DataFakeFactory.java
+++ backend/src/test/java/com/imo/backend/e2e/utils/DataFakeFactory.java
@@ -4,13 +4,13 @@ import net.datafaker.Faker;
 
 public class DataFakeFactory {
 
-    private static Faker faker = new Faker(); 
+    private static Faker faker = new Faker();
 
-    public static String generateValidUsername(){
+    public static String generateValidUsername() {
         return faker.name().firstName() + faker.name().lastName();
     }
 
-    public static String generateValidPassword(){
+    public static String generateValidPassword() {
         return faker.internet().password(8, 50, true, true, true);
     }
 
@@ -24,7 +24,36 @@ public class DataFakeFactory {
 
     public static String generateInvalidEmail(){
         return faker.internet().password(8,
-        50,false,false,true);
+                50, false, false, true);
+    }
+
+    public static String generateValidTitle(){
+        return faker.lorem().characters(10, 50);
+    }
+
+    public static String generateValidDescription(){
+        return faker.educator().course();
+    }
+
+    public static String generateValidYoutubeLink(){
+        String videoId = faker.regexify("[\\w-]{11}");
+        return "https://www.youtube.com/watch?v=" + videoId;
+    }
+
+    public static String generateValidCourseName(){
+        return faker.book().title();
+    }
+
+    public static String generateValidCourseCategory(){
+        return faker.educator().course();
+    }
+
+    public static String generateValidCourseLevel(){
+        return faker.options().option("Iniciante", "Intermediario", "Avancado");
+    }
+
+    public static String generateValidCourseDescription(){
+        return faker.lorem().characters(10,300);
     }
 
 }
