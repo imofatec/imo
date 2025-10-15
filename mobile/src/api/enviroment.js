54753cb60fce311473fdc4be2f3b67f843330895
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-10-15 20:33:38 -0300

    :sparkles: feat: Implementa login e cadastro

---
+++ mobile/src/api/enviroment.js
@@ -0,0 +1,3 @@
+import Constants from 'expo-constants';
+
+export const baseURL = Constants.expoConfig?.extra?.API_URL;
--- mobile/src/app/login.jsx
+++ mobile/src/app/login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Pressable } from "react-native";
 import FormInput from "../components/inputs/formInput";
 import { router } from "expo-router";
@@ -6,18 +6,25 @@ import { Ionicons } from '@expo/vector-icons';
 import { loginSchema } from "../schemas/login";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, Controller } from "react-hook-form";
+import { loginRequest } from "../requests/user/loginRequest";
 
 
 
 export default function Login() {
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const { control, handleSubmit } = useForm({
     resolver: zodResolver(loginSchema)
   })
 
-  function onSubmit(data) {
-    console.log(data);
-    router.replace("/home");
+  async function onSubmit(data) {
+    const response = await loginRequest(data);
+
+    if (!response.success) {
+      setErrorMessage(response.error);
+      return;
+    }
+    router.push("/home");
   }
 
 
@@ -28,13 +35,15 @@ export default function Login() {
         <Ionicons name="caret-forward-outline" size={56} color="white" />IMO
       </Text>
 
-      <FormInput control={control} name="email" label="Email" placeholder="joaosilva@gmail.com" autoCapitalize="none" keyboardType="email-address"/>
-      <FormInput control={control} name="password" label="Senha" placeholder="batatinha123" autoCapitalize="none" secureTextEntry/>
+      <FormInput control={control} name="email" label="Email" placeholder="joaosilva@gmail.com" autoCapitalize="none" keyboardType="email-address" />
+      <FormInput control={control} name="password" label="Senha" placeholder="batatinha123" autoCapitalize="none" secureTextEntry />
 
-
+      {errorMessage && (
+        <Text className="text-red-500 text-center mb-4">{errorMessage}</Text>
+      )}
       <Pressable className="bg-white py-3 rounded-full mb-6"
         onPress={handleSubmit(onSubmit)}>
-          
+
         <Text className="text-black text-2xl text-center font-bold">Entrar</Text>
       </Pressable>
 
