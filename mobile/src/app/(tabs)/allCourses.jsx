80e1fa3aa7a2d48bd6e960ad0ad73dd997f7974b
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-10-28 17:39:30 -0300

    :sparkles: feat: Cria pagina de todos os cursos

--- mobile/src/app/(tabs)/allCourses.jsx
+++ mobile/src/app/(tabs)/allCourses.jsx
@@ -1,9 +1,38 @@
-import react from "react";
-import {View,Text} from "react-native";
+import React from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import { useAllCourses } from "../../hooks/useAllCourses";
+import CourseCard from "../../components/allCourses/courseCard";
+
 export default function AllCourses() {
+  const { courses, loading, error, refetch } = useAllCourses({});
+
+  if (loading) return <ActivityIndicator size="large" color="#000"/>;
+  if (error) return <Text style={{ color: "red" }}>{error}</Text>;
+
   return (
-    <View className="bg-custom-primary flex-1 items-center justify-center">
-        <Text className="text-white">All courses page</Text>
+    <View className="flex-1 bg-custom-primary">
+      <Text className="text-white text-xl text-center py-4">Cursos</Text>
+
+      <FlatList
+        data={courses}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <CourseCard
+            nome={item.name.name}
+            idImg={item.firstLessonYoutubeLink}
+            descricao={item.description}
+          />
+        )}
+        numColumns={2}
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+          paddingHorizontal: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 16 }}
+        refreshing={loading}
+        onRefresh={refetch}
+      />
+      <Text className="text-white text-xl text-center py-4">{`<        1        >`}</Text>
     </View>
   );
 }
