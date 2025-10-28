33f197e817acb9cf157351f0ae1a21f044dc1bfc
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-10-28 17:39:30 -0300

    :sparkles: feat: Cria pagina de todos os cursos

---
+++ mobile/src/components/allCourses/courseCard.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Ionicons } from '@expo/vector-icons';
+import { router } from "expo-router";
+import { View, Text, Image, Dimensions, Pressable} from "react-native";
+import CourseModal from "./courseModal";
+
+export default function CourseCard({ nome, idImg, descricao }) {
+    const [modalVisible, setModalVisible] = useState(false);
+    const { width } = Dimensions.get("window");
+    const imageWidth = width / 2 - 24;
+    const imageHeight = imageWidth * 9 / 16;
+
+    function handleClick() {
+        setModalVisible(false);
+        router.push("/home");
+    }
+
+    return (
+        <View className="flex-1 items-center justify-center my-2">
+            <Pressable onPress={() => setModalVisible(true)}>
+                <Image
+                    source={{
+                        uri: `https://img.youtube.com/vi/${idImg}/maxresdefault.jpg`,
+                    }}
+                    style={{
+                        width: imageWidth,
+                        height: imageHeight,
+                        borderRadius: 8,
+                        borderWidth: 1,
+                        borderColor: "#FFF",
+                    }}
+                    resizeMode="cover"
+                />
+            </Pressable>
+            <Text className="text-white text-center mt-2" numberOfLines={2}>
+                {nome}
+            </Text>
+
+            <CourseModal visible={modalVisible} onClose={() => setModalVisible(false)} nome={nome} idImg={idImg} descricao={descricao} onHandleClick={handleClick}/>
+                
+        </View>
+    );
+}
