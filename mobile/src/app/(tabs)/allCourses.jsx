8b492f6d892295a12903ba013159d9068348e0cc
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-10-30 20:20:26 -0300

    :sparkles: feat: Implementa funcionalidade de buscar cursos no header

--- mobile/src/app/(tabs)/allCourses.jsx
+++ mobile/src/app/(tabs)/allCourses.jsx
@@ -1,5 +1,6 @@
+import { useLocalSearchParams, router } from "expo-router";
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, ActivityIndicator, Pressable } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { useAllCourses } from "../../hooks/useAllCourses";
 import CourseCard from "../../components/allCourses/courseCard";
 import FilterDrawer from "../../components/allCourses/filterDrawer";
@@ -9,13 +10,42 @@ export default function AllCourses() {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [page, setPage] = useState(0);
   const size = 10;
-  const { courses, loading, error, refetch } = useAllCourses({ page, size, categorySlug: selectedCategory || undefined });
+  const params = useLocalSearchParams();
+  const searchTerm = params.search || "";
+
+  const nameSlugToFetch = selectedCategory ? undefined : searchTerm;
+
+  const { courses, loading, error, refetch } = useAllCourses({ page, size, categorySlug: selectedCategory || undefined, nameSlug: nameSlugToFetch, matchType: nameSlugToFetch ? "CONTAINS" : "PERFECT", });
 
   useEffect(() => {
     setPage(0);
+  }, [selectedCategory, searchTerm]);
+
+  useEffect(() => {
+    if (selectedCategory && searchTerm) {
+      router.setParams({ search: undefined });
+    }
   }, [selectedCategory]);
 
-  if (error) return <Text style={{ color: "red" }}>{error}</Text>;
+  useEffect(() => {
+    if (searchTerm && selectedCategory) {
+      setSelectedCategory(null);
+    }
+  }, [searchTerm]);
+
+  const getTitle = () => {
+    if (selectedCategory) return selectedCategory;
+    if (searchTerm) return `Resultados para "${searchTerm}"`;
+    return "Todos os Cursos";
+  };
+
+  if (error) {
+    return (
+      <View className="flex-1 bg-custom-primary justify-center items-center">
+        <Text className="text-red-500">{error}</Text>
+      </View>
+    );
+  }
 
   const isPrevDisabled = page === 0;
   const isNextDisabled = courses.length < size;
@@ -24,7 +54,9 @@ export default function AllCourses() {
   return (
     <View className="flex-1 bg-custom-primary">
       <View className="flex-row items-center justify-between px-4 py-4">
-        <Text className="text-white text-xl font-bold pl-3">{selectedCategory || "Todos os Cursos"}</Text>
+        <Text className="text-white text-xl font-bold pl-3">
+          {getTitle()}
+        </Text>
         <FilterDrawer selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
       </View>
 
--- mobile/src/components/header/searchBar.jsx
+++ mobile/src/components/header/searchBar.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Pressable, TextInput, Animated } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from "expo-router";
 import { useDrawerAnimation } from "../../hooks/useDrawerAnimation";
+import { set } from 'zod';
 
 export default function SearchBar() {
     const [isOpen, setIsOpen] = useState(false);
     const { slideAnim, opacityAnim, closeWithAnimation } = useDrawerAnimation(isOpen);
+    const [query, setQuery] = useState("");
+    const router = useRouter();
+    const inputRef = useRef(null);
 
     const handleClose = () => {
         closeWithAnimation(() => setIsOpen(false));
     };
+
+    const handleSearch = () => {
+        if (!query.trim()) return;
+        handleClose();
+        router.push({
+            pathname: '/allCourses',
+            params: { search: query.trim() },
+        });
+        setQuery("")
+    };
+
+    useEffect(() => {
+        if (isOpen && inputRef.current) {
+            setTimeout(() => {
+                inputRef.current?.focus();
+            }, 100);
+        }
+    }, [isOpen]);
 
     return (
         <>
@@ -33,9 +56,14 @@ export default function SearchBar() {
                         }}
                         className="absolute top-0 left-0 right-0 z-30 p-4">
                         <TextInput
+                            ref={inputRef}
+                            value={query}
+                            onChangeText={setQuery}
+                            onSubmitEditing={handleSearch}
                             className="h-11 w-full bg-custom-primary rounded-full px-4 text-white border border-white"
                             placeholder="Pesquise por um curso..."
                             placeholderTextColor="#fff"
+                            returnKeyType="search"
                         />
                         <Pressable className='absolute flex items-center justify-center top-[19px] right-6 h-8 w-8' onPress={handleClose}>
                             <Ionicons name="close" size={20} color="white" />
