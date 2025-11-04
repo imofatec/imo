3e4aad485b2625293c25660ae2b259c966d50be7
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-11-04 00:47:09 -0300

    :sparkles: Implementa exibição de dados do certificado

--- mobile/src/schemas/certificate.js
+++ mobile/src/schemas/certificate.js
@@ -1,5 +1,5 @@
 import {z} from "zod";
 
 export const certificateSchema = z.object({
-    id: z.string({message: "ID do certificado é obrigatório"}),
+    id: z.string({message: "ID do certificado é obrigatório"}).min(24, {message: "ID do certificado deve ter no mínimo 24 caracteres"})
 });
