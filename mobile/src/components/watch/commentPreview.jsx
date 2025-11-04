491c4d0e00ec5bc43dd9c71ca502237895d3db04
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-11-04 20:18:28 -0300

    :sparkles: Implementa seção e envio de comentarios

--- mobile/src/components/watch/commentPreview.jsx
+++ mobile/src/components/watch/commentPreview.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { View, Text, Pressable } from "react-native";
+import { useUserById } from "../../hooks/useUserById";
 
 export default function CommentsPreview({ latest, count, onPress }) {
+
+  const { user,loading } = useUserById(latest?.userId);
+
   return (
     <Pressable onPress={onPress} className="mt-3 mx-2 p-3 rounded-2xl bg-white/5 border border-white/10">
       <View className="flex-row items-baseline justify-between">
@@ -12,8 +16,8 @@ export default function CommentsPreview({ latest, count, onPress }) {
       {latest ? (
         <>
           <Text className="text-white/80 mt-2 number-of-lines-1">
-            <Text className="text-white font-semibold">{latest.authorName}: </Text>
-            {latest.text}
+            <Text className="text-white font-semibold">{loading ? "Carregando..." : user?.name}:{" "} </Text>
+            {latest.content}
           </Text>
           <Text className="text-white/50 text-custom-header-cyan mt-2">Ver todos</Text>
         </>
