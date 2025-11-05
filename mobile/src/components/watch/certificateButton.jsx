be89d9882342c96405deea10396691dfd434ce71
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-11-05 20:57:21 -0300

    :sparkles: feat: Permite download de certificado de conclusão

--- mobile/src/components/watch/certificateButton.jsx
+++ mobile/src/components/watch/certificateButton.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, ActivityIndicator, View } from "react-native";
 
-export default function CertificateButton({ disabled, onPress }) {
+export default function CertificateButton({ disabled, onPress, loading }) {
   return (
     <Pressable
       onPress={onPress}
-      disabled={disabled}
+      disabled={disabled || loading}
       className={`py-3 rounded-full mb-10 ${
-        disabled ? "bg-gray-500" : "bg-green-400"
+        disabled || loading ? "bg-gray-500" : "bg-green-400"
       }`}
     >
-      <Text className={`text-center text-lg font-bold ${disabled ? "text-white/80" : "text-black"}`}>
-        Gerar Certificado
-      </Text>
+      <View className="flex-row items-center justify-center">
+        {loading && (
+          <ActivityIndicator  size="small" color={disabled ? "#fff" : "#000"} className="mr-2"/>
+        )}
+        <Text className={`text-center text-lg font-bold ${disabled || loading ? "text-white/80" : "text-black"}`}>
+          {loading ? "Gerando..." : "Gerar Certificado"}
+        </Text>
+      </View>
     </Pressable>
   );
 }
