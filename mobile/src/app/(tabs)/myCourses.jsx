ab09d2cb22fb42fea24beb85034d4ac14d792401
matheus <matheus.mariano9@fatec.sp.gov.br>
2025-11-09 18:16:06 -0300

    :sparkles: feat: Cria pagina de meus cursos

--- mobile/src/app/(tabs)/myCourses.jsx
+++ mobile/src/app/(tabs)/myCourses.jsx
@@ -1,9 +1,72 @@
-import react from "react";
-import {View,Text} from "react-native";
-export default function MyCourses() {
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList } from "react-native";
+import { useAllCourses } from "../../hooks/useAllCourses";
+import CourseCard from "../../components/allCourses/courseCard";
+import FilterDrawer from "../../components/myCourses/filterDrawer";
+import PaginationControls from "../../components/allCourses/paginationControls";
+
+export default function AllCourses() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [page, setPage] = useState(0);
+  const size = 10;
+  const { courses, loading, error, refetch } = useAllCourses({ page, size, categorySlug: selectedCategory || undefined, matchType: "PERFECT"});
+
+  useEffect(() => {
+    setPage(0);
+  }, [selectedCategory]);
+
+  const getTitle = () => {
+    if (selectedCategory) return selectedCategory;
+    return "Meus Cursos";
+  };
+
+  if (error) {
+    return (
+      <View className="flex-1 bg-custom-primary justify-center items-center">
+        <Text className="text-red-500">{error}</Text>
+      </View>
+    );
+  }
+
+  const isPrevDisabled = page === 0;
+  const isNextDisabled = courses.length < size;
+
   return (
-    <View className="bg-custom-primary flex-1 items-center justify-center">
-        <Text className="text-white">My Courses page</Text>
+    <View className="flex-1 bg-custom-primary">
+      <View className="flex-row items-center justify-between px-4 py-4">
+        <Text className="text-white text-xl font-bold pl-3">
+          {getTitle()}
+        </Text>
+        <FilterDrawer selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      </View>
+
+      {loading && <Text className="text-white mt-2">Carregando categorias...</Text>}
+
+      {!loading && !error && (
+        <FlatList
+          data={courses}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <CourseCard
+              nome={item.name.name}
+              courseID={item.id}
+              nomeSlug={item.name.slug}
+              idImg={item.firstLessonYoutubeLink}
+              descricao={item.description}
+            />
+          )}
+          numColumns={2}
+          columnWrapperStyle={{
+            justifyContent: "space-between",
+            paddingHorizontal: 16,
+          }}
+          contentContainerStyle={{ paddingBottom: 16 }}
+          refreshing={loading}
+          onRefresh={refetch}
+        />
+      )
+      }
+      <PaginationControls page={page} onPrev={() => setPage(prev => prev - 1)} onNext={() => setPage(prev => prev + 1)} isPrevDisabled={isPrevDisabled} isNextDisabled={isNextDisabled} />
     </View>
   );
 }
