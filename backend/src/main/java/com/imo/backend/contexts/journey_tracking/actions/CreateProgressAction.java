cec14b45335fbc9749727e2e13b94773c9ab8993
matheus <matheus.mariano9@fatec.sp.gov.br>
2026-03-09 16:05:14 -0300

    :recycle: refactor: Ajusta contexto journey_tracking

--- backend/src/main/java/com/imo/backend/contexts/journey_tracking/actions/CreateProgressAction.java
+++
@@ -1,9 +0,0 @@
-package com.imo.backend.contexts.journey_tracking.actions;
-
-import com.imo.backend.contexts.journey_tracking.Progress;
-
-import java.util.List;
-
-public interface CreateProgressAction {
-  Progress execute(String userId, String courseId, List<String> lessonWatched);
-}
