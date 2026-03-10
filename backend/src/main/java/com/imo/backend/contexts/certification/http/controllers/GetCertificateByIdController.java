bb28b833fd73a67a6b0c6e2b7992e8ba26797aae
Vastobode <nikolascruz88@gmail.com>
2026-03-09 21:56:35 -0300

    :recycle: refactor: Ajusta certificação

--- backend/src/main/java/com/imo/backend/contexts/certification/http/controllers/GetCertificateByIdController.java
+++ backend/src/main/java/com/imo/backend/contexts/certification/http/controllers/GetCertificateByIdController.java
@@ -1,7 +1,8 @@
 package com.imo.backend.contexts.certification.http.controllers;
 
 import com.imo.backend.contexts.certification.CertificateDetails;
-import com.imo.backend.contexts.certification.guards.GetCertificateDetailsByIdGuard;
+import com.imo.backend.contexts.certification.repositories.CertificateRepository;
+import com.imo.backend.contexts.common.MongoDB;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetCertificateByIdController extends CertificateController {
-  private final GetCertificateDetailsByIdGuard getCertificateDetailsByIdGuard;
+  private final CertificateRepository certificateRepository;
 
-  public GetCertificateByIdController(GetCertificateDetailsByIdGuard getCertificateDetailsByIdGuard) {
-    this.getCertificateDetailsByIdGuard = getCertificateDetailsByIdGuard;
+  public GetCertificateByIdController(CertificateRepository certificateRepository) {
+    this.certificateRepository = certificateRepository;
   }
 
   @Operation(summary = "Get certificate details by id")
@@ -22,6 +23,7 @@ public class GetCertificateByIdController extends CertificateController {
       @PathVariable
       String id
   ) {
-    return ResponseEntity.ok(this.getCertificateDetailsByIdGuard.execute(id));
+    MongoDB.validateObjectId(id);
+    return ResponseEntity.ok(this.certificateRepository.findDetailsByIdOrThrow(id));
   }
 }
