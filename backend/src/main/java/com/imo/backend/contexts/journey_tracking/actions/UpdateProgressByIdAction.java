a4e3a345f2804b5ae1085ce0e43d3447ce03c167
matheus <matheus.mariano9@fatec.sp.gov.br>
2026-03-15 01:41:29 -0300

    :recycle: refactor: extingue actions movendo logica para orchestrator

--- backend/src/main/java/com/imo/backend/contexts/journey_tracking/actions/UpdateProgressByIdAction.java
+++
@@ -1,8 +0,0 @@
-package com.imo.backend.contexts.journey_tracking.actions;
-
-import com.imo.backend.contexts.journey_tracking.Progress;
-import com.imo.backend.contexts.journey_tracking.commands.UpdateProgressCommand;
-
-public interface UpdateProgressByIdAction {
-  Progress execute(String id, UpdateProgressCommand cmd);
-}
