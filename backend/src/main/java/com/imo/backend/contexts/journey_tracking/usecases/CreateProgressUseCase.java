86263379894f91b631b773e0a7db0952b1445138
matheus <matheus.mariano9@fatec.sp.gov.br>
2026-03-17 00:55:16 -0300

    :recycle: refactor: remove logica do orchestrator e move para o usecase

--- backend/src/main/java/com/imo/backend/contexts/journey_tracking/usecases/CreateProgressUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.imo.backend.contexts.journey_tracking.usecases;
-
-import com.imo.backend.contexts.journey_tracking.Progress;
-
-public interface CreateProgressUseCase {
-  Progress execute(String userId, String courseId, String firstLessonId);
-}
--- backend/src/main/java/com/imo/backend/contexts/journey_tracking/usecases/WatchLessonByIdUseCase.java
+++ backend/src/main/java/com/imo/backend/contexts/journey_tracking/usecases/WatchLessonByIdUseCase.java
@@ -1,36 +1,35 @@
-package com.imo.backend.contexts.journey_tracking.orchestrators;
+package com.imo.backend.contexts.journey_tracking.usecases;
 
 import com.imo.backend.contexts.catalog.course.Course;
 import com.imo.backend.contexts.catalog.course.repositories.CourseRepository;
-import com.imo.backend.contexts.catalog.lesson.Lesson;
-import com.imo.backend.contexts.catalog.lesson.repositories.LessonRepository;
 import com.imo.backend.contexts.common.exceptions.custom.NotFoundException;
 import com.imo.backend.contexts.journey_tracking.Progress;
 import com.imo.backend.contexts.journey_tracking.commands.UpdateProgressCommand;
 import com.imo.backend.contexts.journey_tracking.controllers.dtos.ProgressDTO;
 import com.imo.backend.contexts.journey_tracking.repositories.ProgressRepository;
-import com.imo.backend.contexts.journey_tracking.usecases.CreateProgressUseCase;
+import com.imo.backend.contexts.journey_tracking.policies.ProgressPolicy;
 import com.imo.backend.contexts.journey_tracking.value_objects.ProgressStatus;
 import org.springframework.stereotype.Service;
 
-@Service
-public class WatchLessonByIdOrchestratorImpl implements WatchLessonByIdOrchestrator {
-  private final ProgressRepository progressRepository;
+import java.util.List;
 
-  private final CreateProgressUseCase createProgressUseCase;
+@Service
+public class WatchLessonByIdUseCase {
+  private final ProgressRepository progressRepository;
 
   private final CourseRepository courseRepository;
 
-  public WatchLessonByIdOrchestratorImpl(
+  private final ProgressPolicy policy;
+
+  public WatchLessonByIdUseCase(
       ProgressRepository progressRepository,
-      CreateProgressUseCase createProgressUseCase,
-      CourseRepository courseRepository) {
+      CourseRepository courseRepository,
+      ProgressPolicy policy) {
     this.progressRepository = progressRepository;
-    this.createProgressUseCase = createProgressUseCase;
     this.courseRepository = courseRepository;
+    this.policy = policy;
   }
 
-  @Override
   public ProgressDTO execute(String lessonId, String userId) {
     Progress currentProgress = null;
 
@@ -46,10 +45,17 @@ public class WatchLessonByIdOrchestratorImpl implements WatchLessonByIdOrchestra
     assert existingCourse != null;
 
     if (currentProgress == null) {
-      return ProgressDTO.fromProgress(this.createProgressUseCase.execute(
+      String courseId = existingCourse.getId();
+      Course course = this.courseRepository.findByIdOrThrow(courseId);
+      Progress progress = this.policy.startProgress(
           userId,
-          existingCourse.getId(),
-          lessonId));
+          courseId,
+          List.of(lessonId),
+          course.getLessonsCount());
+
+      Progress newProgress = this.progressRepository.save(progress);
+
+      return ProgressDTO.fromProgress(newProgress);
     }
 
     if (currentProgress.getStatus() == ProgressStatus.FINISHED) {
@@ -67,4 +73,5 @@ public class WatchLessonByIdOrchestratorImpl implements WatchLessonByIdOrchestra
 
     return ProgressDTO.fromProgress(updatedProgress);
   }
+
 }
