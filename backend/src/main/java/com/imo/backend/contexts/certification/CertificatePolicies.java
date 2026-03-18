1e60f275362c7da95275f9b9eb5af0bcbe7b0095
Vastobode <nikolascruz88@gmail.com>
2026-03-18 19:36:58 -0300

    :recycle: refactor: Move regra de negócio de uma action para uma policy

---
+++ backend/src/main/java/com/imo/backend/contexts/certification/CertificatePolicies.java
@@ -0,0 +1,30 @@
+package com.imo.backend.contexts.certification;
+
+import org.springframework.stereotype.Service;
+
+import com.imo.backend.contexts.common.exceptions.custom.ForbiddenException;
+import com.imo.backend.contexts.common.exceptions.custom.NotFoundException;
+import com.imo.backend.contexts.journey_tracking.Progress;
+import com.imo.backend.contexts.journey_tracking.repositories.ProgressRepository;
+import com.imo.backend.contexts.journey_tracking.value_objects.ProgressStatus;
+
+@Service
+public class CertificatePolicies {
+  private final ProgressRepository progressRepository;
+
+  public CertificatePolicies(ProgressRepository progressRepository) {
+    this.progressRepository = progressRepository;
+  }
+
+  public Progress assertCourseIsFinished(String userId, String courseId) {
+    Progress progress = this.progressRepository
+        .findByUserIdAndCourseId(userId, courseId)
+        .orElseThrow(() -> new NotFoundException("Progresso não encontrado."));
+
+    if (progress.getStatus() != ProgressStatus.FINISHED) {
+      throw new ForbiddenException("Finalize o curso para emitir o certificado.");
+    }
+
+    return progress;
+  }
+}
