6b70750e289825b1298d89d9ad760f171669e9a9
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2026-03-21 23:06:27 -0300

    :sparkles: feat: implementa entrega de email para recuperar senha

--- backend/src/main/java/com/imo/backend/contexts/identity/events/ForgetPasswordEvent.java
+++ backend/src/main/java/com/imo/backend/contexts/identity/events/ForgetPasswordEvent.java
@@ -2,6 +2,7 @@ package com.imo.backend.contexts.identity.events;
 
 public record ForgetPasswordEvent(
     String userId,
+    String name,
     String email,
     String code
 ) {
---
+++ backend/src/main/java/com/imo/backend/contexts/notification/ForgetPasswordConsumer.java
@@ -0,0 +1,45 @@
+package com.imo.backend.contexts.notification;
+
+import com.imo.backend.contexts.identity.events.ForgetPasswordEvent;
+import com.imo.backend.contexts.notification.lib.MailManager;
+import com.imo.backend.contexts.notification.lib.MailMessageBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class ForgetPasswordConsumer {
+  private final MailManager mailManager;
+
+  private static final String EMAIL_SUBJECT = "IMO - Recuperar senha";
+  private static final String TEMPLATE_NAME = "forget_password";
+
+  public ForgetPasswordConsumer(
+      @Qualifier("javaMailManager")
+      MailManager mailManager
+  ) {
+    this.mailManager = mailManager;
+  }
+
+  @RabbitListener(queues = "${rabbitmq.queue.forget_password}")
+  public void handle(ForgetPasswordEvent event) {
+    log.debug("FORGET_PASSWORD_EVENT: mensagem recebida {}", event.code());
+
+    MailMessageBuilder messageBuilder = new MailMessageBuilder()
+        .setTo(event.email())
+        .setSubject(EMAIL_SUBJECT);
+
+    List<String> digits = event.code().chars().mapToObj(c -> String.valueOf((char) c)).toList();
+
+    Map<String, Object> templateVariables = Map.of("digits", digits, "name", event.name());
+
+    log.debug("FORGET_PASSWORD_EVENT: template configurado");
+
+    this.mailManager.sendWithTemplate(messageBuilder, TEMPLATE_NAME, templateVariables);
+  }
+}
