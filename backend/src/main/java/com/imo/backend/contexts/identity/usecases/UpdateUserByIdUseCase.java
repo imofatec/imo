7bd1685eaeb4a35fa48df1e1f204f5d61c1b11fc
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2026-03-21 22:28:59 -0300

    :wastebasket: remove: apaga redundancias e ruidos

--- backend/src/main/java/com/imo/backend/contexts/identity/usecases/UpdateUserByIdUseCase.java
+++ backend/src/main/java/com/imo/backend/contexts/identity/usecases/UpdateUserByIdUseCase.java
@@ -4,32 +4,28 @@ import com.imo.backend.contexts.identity.User;
 import com.imo.backend.contexts.identity.commands.UpdateUserByIdCommand;
 import com.imo.backend.contexts.identity.http.dtos.UpdateUserByIdRequest;
 import com.imo.backend.contexts.identity.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UpdateUserByIdUseCase {
 
-  private final UpdatePasswordByIdUseCase updatePasswordByIdUseCase;
+  private final PasswordEncoder passwordEncoder;
 
   private final UserRepository userRepository;
 
-  public UpdateUserByIdUseCase(
-      UpdatePasswordByIdUseCase updatePasswordByIdUseCase,
-      UserRepository userRepository
-  ) {
-    this.updatePasswordByIdUseCase = updatePasswordByIdUseCase;
+  public UpdateUserByIdUseCase(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    this.passwordEncoder = passwordEncoder;
     this.userRepository = userRepository;
   }
 
   public User execute(String id, UpdateUserByIdRequest fieldsToUpdateUser) {
-    if (fieldsToUpdateUser.password() != null) {
-      this.updatePasswordByIdUseCase.execute(id, fieldsToUpdateUser.password());
-    }
-
     UpdateUserByIdCommand cmd = new UpdateUserByIdCommand(
         fieldsToUpdateUser.email(),
         fieldsToUpdateUser.name(),
-        null,
+        fieldsToUpdateUser.password() != null
+            ? this.passwordEncoder.encode(fieldsToUpdateUser.password())
+            : null,
         null,
         User.getBirthDateFromString(fieldsToUpdateUser.birthDate()),
         fieldsToUpdateUser.availableTimePerDay(),
