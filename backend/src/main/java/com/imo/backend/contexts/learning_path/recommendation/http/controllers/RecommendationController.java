2f21ddec5e95574813bb1e67abcb65de241c59df
Abner Cerqueira <abner.cerqueira@fatec.sp.gov.br>
2026-04-29 21:08:38 -0300

    :books: docs(learning path): ajusta docs endpoint

--- backend/src/main/java/com/imo/backend/contexts/learning_path/recommendation/http/controllers/RecommendationController.java
+++ backend/src/main/java/com/imo/backend/contexts/learning_path/recommendation/http/controllers/RecommendationController.java
@@ -3,6 +3,6 @@ package com.imo.backend.contexts.learning_path.recommendation.http.controllers;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Tag(name = "Recommendation", description = "Endpoints de recomendação de cursos")
+@Tag(name = "Learning path - Recommendation", description = "Endpoints de recomendação de cursos")
 @RequestMapping("/api/recommendation")
 public abstract class RecommendationController {}
--- backend/src/main/java/com/imo/backend/contexts/learning_path/skill_profile/http/controllers/SkillProfileController.java
+++ backend/src/main/java/com/imo/backend/contexts/learning_path/skill_profile/http/controllers/SkillProfileController.java
@@ -3,6 +3,8 @@ package com.imo.backend.contexts.learning_path.skill_profile.http.controllers;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Tag(name = "Skill Profile", description = "Endpoints de perfil de skills do usuário")
+@Tag(
+    name = "Learning path - Skill Profile",
+    description = "Endpoints de perfil de skills do usuário")
 @RequestMapping("/api/skill-profile")
 public abstract class SkillProfileController {}
